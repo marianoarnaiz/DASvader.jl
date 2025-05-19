511c95e7e7a6de4b897cfb786522c9c4cb89f06b
Mariano Arnaiz <40127582+marianoarnaiz@users.noreply.github.com>
2025-05-19 18:15:58 +0200

    Update DASVader.jl

--- src/DASVader.jl
+++ src/DASVader.jl
@@ -32,26 +32,26 @@ end
 #############################################
 # Life line call!
 
-#include("headless.jl")
+include("headless2.jl")
 #using .HeadlessCheck
 #
-#if has_graphical_display()
-#    include("vizGL.jl")
-#    #using .vizGL
-#    @info "Working on graphical mode"
-#else
-#    include("vizCairo.jl")
-#    #using .vizCairo
-#    @info "Working on headless mode"
-#end
+if has_graphical_display()
+    include("vizGL.jl")
+    #using .vizGL
+    @info "Working on graphical mode"
+else
+    include("vizCairo.jl")
+    #using .vizCairo
+    @info "Working on headless mode"
+end
 
-#include("tandf.jl")
-#include("wmatrix.jl")
-#include("filemanagement.jl")
-#include("myspectra.jl")
-#include("xandw.jl")
-#include("detectiontools.jl")
-#include("Findpeaks.jl")
-#include("myspectogram.jl")
-#include("datasrc.jl")
-#include("interface.jl")
+include("tandf.jl")
+include("wmatrix.jl")
+include("filemanagement.jl")
+include("myspectra.jl")
+include("xandw.jl")
+include("detectiontools.jl")
+include("Findpeaks.jl")
+include("myspectogram.jl")
+include("datasrc.jl")
+include("interface.jl")
